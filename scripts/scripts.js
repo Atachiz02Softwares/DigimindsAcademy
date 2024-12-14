5f3807e6665a983ad571f324fca6ff2b62f26616
MOPHE <ndachimya@gmail.com>
2024-12-14 17:21:10 +0100

    UI Improvements...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,35 @@
-import AOS from 'aos';
+let slideIndex = 1;
+const slides = document.getElementsByClassName("testimonial");
 
-// Scroll animations
-document.addEventListener('DOMContentLoaded', function () {
-    AOS.init({
-        duration: 1200,
+const showSlides = (n) => {
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    Array.from(slides).forEach((slide, index) => {
+        slide.classList.remove("active", "prev", "next");
+        if (index === slideIndex - 1) {
+            slide.classList.add("active");
+        } else if (index === slideIndex - 2 || (slideIndex === 1 && index === slides.length - 1)) {
+            slide.classList.add("prev");
+        } else if (index === slideIndex || (slideIndex === slides.length && index === 0)) {
+            slide.classList.add("next");
+        }
     });
-});
+};
+
+const nextSlide = () => {
+    showSlides(slideIndex += 1);
+};
+
+const prevSlide = () => {
+    showSlides(slideIndex -= 1);
+};
+
+document.querySelector('.fa-chevron-right').addEventListener('click', nextSlide);
+document.querySelector('.fa-chevron-left').addEventListener('click', prevSlide);
+
+showSlides(slideIndex);
